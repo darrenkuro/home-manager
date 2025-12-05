e6cc881b860ef3a08e026de1f41fe20a425255e6
darrenkuro <odon5ht@gmail.com>
2025-12-05 05:02:17 +0100

    Update git info

--- home/entry.nix
+++ home/entry.nix
@@ -95,6 +95,18 @@
     config = { theme = "OneDark"; };
   };
 
+  programs.git = {
+    enable = true;
+    #user.email = if tag == "ft" then "dlu@student.42berlin.de" else "odon5ht@gmail.com";
+    settings = {
+      user.name = "darrenkuro";
+      user.email = "odon5ht@gmail.com";
+      core.editor = "hx";
+      color.ui = "auto";
+      init.defaultBranch = "main";
+    };
+  };
+
   home.file.".config/lf/preview.sh" = {
     text = ''
       #!/usr/bin/env bash
