48d451ef965efbd8fb8b72cbf619a514804d1802
dlu <odon5ht@gmail.com>
2025-12-05 03:24:27 +0100

    Fix tmux and nix shell

--- home/entry.nix
+++ home/entry.nix
@@ -4,20 +4,9 @@
   imports = [
     ./alias.nix
    ];
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "25.11"; # Please read the comment before changing.
+  home.stateVersion = "25.11";
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = with pkgs; [
     starship
     helix
@@ -45,14 +34,11 @@
     jq
     tmux
     lf
-
+    bat
     # exiftool, ffuf
     # cmake
     # yarn, wget, yt-dlp, tor
     
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
@@ -83,22 +69,6 @@
     # '';
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/darrenlu/etc/profile.d/hm-session-vars.sh
-  #
   home.sessionVariables = {
     # Home Directory Hygenie
     XDG_CONFIG_HOME = "$HOME/.config";
@@ -106,10 +76,12 @@
     XDG_DATA_HOME = "$HOME/.local/share";
     XDG_STATE_HOME = "$HOME/.local/state";
 
-    
     DROPBOX = "/Users/darrenlu/Dropbox";
     DEV = "$HOME/Documents/dev";
-    # EDITOR = "emacs";
+    NIX = "$HOME/.config/home-manager";
+    
+    EDITOR = "hx";
+    VISUAL = "hx";
   };
 
   # Let Home Manager install and manage itself.
@@ -148,4 +120,29 @@
     name = "cpp"
     language-servers = ["clangd"]
   '';
+
+  programs.lf = {
+    enable = true;
+    settings = {
+      icons = true;
+      preview = true;
+      previewer = "~/.config/lf/preview.sh";
+    };
+
+    extraConfig = ''
+      cmd open $${EDITOR:-hx} "$f"
+      cmd open-multi $${EDITOR:-hx} "$fx"
+
+      map <enter> open
+      map o open-multi
+      map s split
+      cmd split tmux split-window -v hx "$f"
+    '';
+  };
+
+  home.file.".config/lf/preview.sh".text = ''
+    #!/usr/bin/env bash
+    bat --color=always --style=plain "$1"
+  '';
+
 }
