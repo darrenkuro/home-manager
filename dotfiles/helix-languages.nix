a9bd37a4496f37b9e0b0a40d1c5fcc638f285358
darrenkuro <odon5ht@gmail.com>
2025-12-06 09:24:27 +0100

    Convert toml files to nix

---
+++ dotfiles/helix-languages.nix
@@ -0,0 +1,15 @@
+{
+	language = [ {
+		name = "typescript";
+		language-servers = [ "typescript-language-server" ];
+	} {
+		name = "rust";
+		language-servers = [ "rust-analyzer" ];
+	} {
+		name = "c";
+		language-servers = [ "clangd" ];
+	} {
+		name = "cpp";
+		language-servers = [ "clangd" ];
+	} ];
+}
