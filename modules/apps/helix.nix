8938fb13be7d57724e3e4f4efb6d4c087a94ed78
darrenkuro <odon5ht@gmail.com>
2025-12-07 14:31:54 +0100

    Update helix langauge syntax

--- modules/apps/helix.nix
+++ modules/apps/helix.nix
@@ -18,24 +18,26 @@
       };
 	};
 
-    languages = [
-        {
-          name = "typescript";
-          language-servers = [ "typescript-language-server" ];
-        }
-        {
-          name = "rust";
-          language-servers = [ "rust-analyzer" ];
-        }
-        {
-          name = "c";
-          language-servers = [ "clangd" ];
-        }
-        {
-          name = "cpp";
-          language-servers = [ "clangd" ];
-        }
-      ];
+    languages = {
+		language = [
+			{
+			name = "typescript";
+			language-servers = [ "typescript-language-server" ];
+			}
+			{
+			name = "rust";
+			language-servers = [ "rust-analyzer" ];
+			}
+			{
+			name = "c";
+			language-servers = [ "clangd" ];
+			}
+			{
+			name = "cpp";
+			language-servers = [ "clangd" ];
+			}
+		];
+	  };
     };
 }
 
