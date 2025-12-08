09859d8c00765334a210066a6e96ab628d495201
darrenkuro <odon5ht@gmail.com>
2025-12-08 13:27:17 +0100

    Add rust set up

--- modules/dev/rust.nix
+++ modules/dev/rust.nix
@@ -1,7 +1,54 @@
-{pkgs, ...}: {
+{ pkgs, ... }:
+{
   home.packages = with pkgs; [
-    rustc
-    cargo
-    rust-analyzer
+    rustc # Compiler
+    cargo # Package manager & Build tool
+    rust-analyzer # LSP
+    rustfmt # Formatter
+
+    clippy
   ];
+
+  programs.helix.languages = {
+    language = [
+      {
+        name = "rust";
+
+        language-server = {
+          command = "rust-analyzer";
+        };
+
+        auto-format = true;
+        formatter = {
+          command = "rustfmt";
+          args = [
+            "--emit"
+            "files"
+          ]; # rewrite files in place
+        };
+      }
+    ];
+  };
+
+  programs.vscode = {
+
+    profiles.default = {
+      extensions = with pkgs.vscode-extensions; [
+        rust-lang.rust-analyzer
+      ];
+
+      userSettings = {
+        "rust-analyzer.server.path" = "rust-analyzer";
+
+        "[rust]" = {
+          "editor.formatOnSave" = true;
+          "editor.defaultFormatter" = "rust-lang.rust-analyzer";
+        };
+
+        "rust-analyzer.cargo.autoreload" = true;
+        "rust-analyzer.procMacro.enable" = true;
+        "rust-analyzer.checkOnSave.command" = "clippy";
+      };
+    };
+  };
 }
