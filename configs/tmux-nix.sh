0277c3ab8442931a9d1f2f3b48f89f51182d6f1d
darrenkuro <odon5ht@gmail.com>
2025-12-23 01:40:35 +0100

    Update tmux-nix

--- configs/tmux-nix.sh
+++ configs/tmux-nix.sh
@@ -4,7 +4,6 @@ set -euo pipefail
 
 NUC="${NUC:-/home/dlu/bin/nix-user-chroot}"
 NIXROOT="${NIXROOT:-\"$HOME/sgoinfre/nix\"}"
-[[ ! $(command -v nix) && -e '. ~/.nix-profile/etc/profile.d/nix.sh' ]] && source '~/.nix-profile/etc/profile.d/nix.sh'
 
 # If already in a tmux session
 if [[ -n "${TMUX-}" ]]; then
