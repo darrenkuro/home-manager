f988767f2ec798f9392f4f0591f062352c0fa146
dlu <odon5ht@gmail.com>
2025-12-23 04:16:05 +0100

    Add some aliases

--- modules/system/aliases.nix
+++ modules/system/aliases.nix
@@ -6,19 +6,25 @@
   programs.zsh.shellAliases = lib.mkMerge [
     # ---- common aliases (always enabled)
     {
+      objdump = "objdump --disassembler-options=intel";
       ls = "eza --icons --ignore-glob='.DS_Store'";
       cloc = "tokei";
 
       a = "hx $HM/modules/system/aliases.nix"; # Aliases (shared)
 
-      dev = "cd $DEV";
+      # Faster navigation
+      devg = "cd $DEV"; # dev go
+      hmg = "cd $HM"; # hm go
+
 
       ncg = "nix-collect-garbage -d";
-      objdump = "objdump --disassembler-options=intel";
       clean = "rm -rf $HOME/.npm $HOME/.zcompdump $HOME/.cache $HOME/.lesshst";
 
       tpll = "git -C $XDG_DATA_HOME/task pull";
       tpsh = "git -C $XDG_DATA_HOME/task add . && git -C $XDG_DATA_HOME/task commit -m 'Auto-sync' && git -C $XDG_DATA_HOME/task push";
+
+      hmpsh = "git -C $HM add . && git -C $HM commit -m 'Auto-push' && git -C $HM push";
+      hmpll = "git -C $HM pull";
     }
 
     # ---- mac-only aliases
@@ -36,7 +42,7 @@
     # ---- ft-only aliases
     (lib.mkIf (tag == "ft") {
       p = "hx $HM/modules/system/linux-ft.nix";
-      hm = "cd $HM";
+      hm = "code --no-sandbox $HM";
       re = "home-manager switch --flake $HM#ft && exec zsh";
 
       code = "code --no-sandbox"; # VSCode requires --no-sandbox to run in nix env on 42
