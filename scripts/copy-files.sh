e13d496d4936b160c48a5c0f1bfc996efe066f15
darrenkuro <odon5ht@gmail.com>
2025-12-23 01:40:13 +0100

    Improve prettier-config

--- scripts/copy-files.sh
+++ scripts/copy-files.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/env bash
-
 # Copy files in Place since these are important outside of nix env
 case ${HM_TAG-} in
   MAC) VSCODE_DIR="$HOME/Library/Application Support/Code/User" ;;
@@ -13,20 +11,20 @@ envsubst < "$HM/configs/vscode-settings.jsonc" > "$VSCODE_DIR/settings.json"
 chmod u+w "$VSCODE_DIR/settings.json"
 
 if [[ ${HM_TAG-} == "FT" ]]; then
-    # Alacritty
-    mkdir -p "$HOME/.config/alacritty"
-    envsubst < "$HM/configs/alacritty.toml" > "$HOME/.config/alacritty/alacritty.toml"
-    chmod u+w "$HOME/.config/alacritty/alacritty.toml"
+  # Alacritty
+  mkdir -p "$HOME/.config/alacritty"
+  envsubst < "$HM/configs/alacritty.toml" > "$HOME/.config/alacritty/alacritty.toml"
+  chmod u+w "$HOME/.config/alacritty/alacritty.toml"
 
-    # tmux
-    mkdir -p "$HOME/.config/tmux"
-    envsubst < "$HM/configs/tmux.conf" > "$HOME/.config/tmux/tmux.conf"
-    chmod u+w "$HOME/.config/tmux/tmux.conf"
-    tmux_conf="$HOME/.config/tmux/tmux.conf"
-    tmp_conf="$tmux_conf.tmp"
-    printf '%s\n' "set -g default-command 'exec /home/dlu/bin/nix-user-chroot "$HOME/sgoinfre/nix" zsh -l'" | cat - "$tmux_conf" > "$tmp_conf" && mv "$tmp_conf" "$tmux_conf"
+  # tmux
+  mkdir -p "$HOME/.config/tmux"
+  envsubst < "$HM/configs/tmux.conf" > "$HOME/.config/tmux/tmux.conf"
+  chmod u+w "$HOME/.config/tmux/tmux.conf"
+  tmux_conf="$HOME/.config/tmux/tmux.conf"
+  tmp_conf="$tmux_conf.tmp"
+  printf '%s\n' "set -g default-command 'exec /home/dlu/bin/nix-user-chroot "$HOME/sgoinfre/nix" zsh -l'" | cat - "$tmux_conf" > "$tmp_conf" && mv "$tmp_conf" "$tmux_conf"
 
-    # bin for autoload tmux and nix
-    cat "$HM/configs/tmux-nix.sh" > "/home/dlu/bin/tmux-nix"
-    chmod +x "/home/dlu/bin/tmux-nix"
+  # bin for autoload tmux and nix
+  cat "$HM/configs/tmux-nix.sh" > "/home/dlu/bin/tmux-nix"
+  chmod +x "/home/dlu/bin/tmux-nix"
 fi
