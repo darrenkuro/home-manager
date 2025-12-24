190c795376c03a7d77c816d3914ef95e06cabfce
darrenkuro <odon5ht@gmail.com>
2025-12-24 18:59:09 +0100

    Add explanation for man alias

--- modules/system/aliases-man.nix
+++ modules/system/aliases-man.nix
@@ -1,3 +1,11 @@
+/*
+  Since nix-profile are prepended to the very beginning of path,
+  man is replaced as well, and nix does not know SDK env that apple
+  append at runtime; the system ones /usr/bin/man can find any manual
+  in nix env though, so this setup should be ideal.
+  Decided not to switch the order in PATH since we could change cmd that is
+  in /usr/bin for consistency across system in the future such as grep.
+*/
 { ... }:
 {
   programs.zsh.shellAliases = {
