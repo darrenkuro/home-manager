741887a71b0d83fd643595885f23471a70c6ca6a
darrenkuro <odon5ht@gmail.com>
2026-09-17 19:52:16 +0200

    Add comments and more personal apps for work command

--- functions/work.sh
+++ functions/work.sh
@@ -3,16 +3,23 @@ REQUIRED_TOOLS=()
 _check_preamble || return 0
 
 work() {
+    # Clear clipboard
     pbcopy < /dev/null
+
+    # Hide Finder dotfiles
     defaults write com.apple.finder AppleShowAllFiles -bool false
     killall Finder
 
+    # Close personal browsers
     osascript -e 'tell application "Safari" to quit'
     osascript -e 'tell application "Brave Browser" to quit'
+    osascript -e 'tell application "QuickTime Player" to quit'
 
-    while pgrep -x "Brave Browser" >/dev/null; do
+    # Wait until both have actually exited
+    while pgrep -x "Safari" >/dev/null || pgrep -x "Brave Browser" >/dev/null; do
         sleep 0.2
     done
 
+    # Launch Brave Work profile
     open -a "Brave Browser.app" --args --profile-directory="Profile 1"
 }
